87c047d50c86c791f594c6fb9458f767dd803992
Glitch (shared-economic-gymnast) <none>
2025-06-09 22:25:49 +0400

    views

--- router/gameRouter.js
+++ router/gameRouter.js
@@ -13,7 +13,6 @@ router.get('/:urlTitle',async (req,res)=>{
     .then(async (game)=>{
        if(req.cookies.viewed != urlTitle){
         res.cookie("viewed", urlTitle, {maxAge: 30 * 24 * 3600 * 1000,value: urlTitle})
-        const views = await game.views +1
         var gameUp = await Game.findOneAndUpdate({urlTitle:urlTitle},{views:game.views+1},{new:true})
         
        }
